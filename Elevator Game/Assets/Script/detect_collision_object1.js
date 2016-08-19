fc7c04955c5bdd466f05546e2622cdf0b61a992c
wanaka <jonas.haag@hotmail.com>
2016-08-19 16:33:24 +0200

    added score UI + js files

---
+++ Elevator Game/Assets/Script/detect_collision_object1.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+
+public var otherGameObject :  GameObject;
+
+private var detectCollisionObject0 : detect_collision_object0;
+
+function Start () {
+	detectCollisionObject0 = otherGameObject.GetComponent(detect_collision_object0);
+}
+
+function Update () {}
+
+function OnTriggerEnter (other : Collider) {
+
+	//check if the correct block enter and add one point. Otherwise remove a life.
+		if(other.gameObject.tag == "blueColor"){
+			detectCollisionObject0.points+=detectCollisionObject0.addPoints;
+			    Debug.Log("blueColor");
+			    	   // Debug.Log("Reading another script works!!! " + detectCollisionObject0.points);
+
+			detectCollisionObject0.checkCollision = true;
+		} else{
+			Debug.Log("NOO wrong blockssss");
+			detectCollisionObject0.checkCollision = false;
+		}
+		
+		if(detectCollisionObject0.checkCollision){
+			Debug.Log(detectCollisionObject0.points + "point"); 
+		}
+}
---
+++ Elevator Game/Assets/scoreText.js
@@ -0,0 +1,19 @@
+﻿#pragma strict
+
+private var scoreText : GUIText;
+private  var score : int;
+
+function Start () {
+    score = 0;
+    UpdateScore ();
+}
+
+
+function AddScore (newScoreValue : int) {
+    score += newScoreValue;
+    UpdateScore ();
+}
+
+function UpdateScore () {
+    scoreText.text ="Score: " + score;
+}
